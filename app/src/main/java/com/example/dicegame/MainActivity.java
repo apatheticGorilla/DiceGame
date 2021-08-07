954d69759d1ffadf5db2f5edaca3dbac4cee6f85
apatheticGorilla <83184263+apatheticGorilla@users.noreply.github.com>
2021-08-06 21:06:09 -0600

    updated gradle plugin and dependencies

--- app/src/main/java/com/example/dicegame/MainActivity.java
+++ app/src/main/java/com/example/dicegame/MainActivity.java
@@ -1,17 +1,16 @@
 package com.example.dicegame;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-	int[] imageIndex = {R.drawable.one, R.drawable.two, R.drawable.three, R.drawable.four,R.drawable.five, R.drawable.six};
+	int[] imageIndex = {R.drawable.one, R.drawable.two, R.drawable.three, R.drawable.four, R.drawable.five, R.drawable.six};
 	int[] rolls = new int[3];
 	ImageView diceOne, diceTwo, diceThree;
 	
@@ -23,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 		diceTwo = findViewById(R.id.dice2);
 		diceThree = findViewById(R.id.dice3);
 	}
+	
 	@SuppressWarnings("unused")
 	public void onRoll(View view) {
 		Random random = new Random();
@@ -31,18 +31,21 @@ public class MainActivity extends AppCompatActivity {
 		diceTwo.setImageResource(imageIndex[rolls[1]]);
 		diceThree.setImageResource(imageIndex[rolls[2]]);
 		System.out.println(checkToastConditions());
-		if(checkToastConditions()==1) Toast.makeText(this, "Two Of A Kind!", Toast.LENGTH_SHORT).show();
-		if(checkToastConditions()==2) Toast.makeText(this, "Three Of A Kind!", Toast.LENGTH_SHORT).show();
+		if (checkToastConditions() == 1)
+			Toast.makeText(this, "Two Of A Kind!", Toast.LENGTH_SHORT).show();
+		if (checkToastConditions() == 2)
+			Toast.makeText(this, "Three Of A Kind!", Toast.LENGTH_SHORT).show();
 	}
+	
 	//1 = two of a kind; 2 = three of a kind; 0 = no match
 	int checkToastConditions() {
-		StringBuilder string= new StringBuilder();
-		for (int roll : rolls) string.append(roll);
 		
-		if(Integer.parseInt(string.toString())%111==0)return 2;
-		if(rolls[0]==rolls[1]) return 1;
-		if(rolls[1]==rolls[2]) return 1;
-		if(rolls[0]==rolls[2]) return 1;
+		StringBuilder string = new StringBuilder();
+		for (int roll : rolls) string.append(roll);
+		if (Integer.parseInt(string.toString()) % 111 == 0) return 2;
+		if (rolls[0] == rolls[1]) return 1;
+		if (rolls[1] == rolls[2]) return 1;
+		if (rolls[0] == rolls[2]) return 1;
 		return 0;
 	}
 }
